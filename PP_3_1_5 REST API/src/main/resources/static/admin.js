ccbac70b4ea639313a869160a9fe90222df446e2
Yana <yana_mashuta@mail.ru>
2023-12-07 11:25:20 +0500

    3

--- PP_3_1_5 REST API/src/main/resources/static/admin.js
+++ PP_3_1_5 REST API/src/main/resources/static/admin.js
@@ -1,42 +1,41 @@
-
-const urlInfo = 'http://localhost:8080/api/users/admin_info'
-const navbarBrandAdmin = document.getElementById('navbarBrandAdmin') //хедер
-const adminTable = document.getElementById('tableAdmin') //личная инфа админа
-//таблица юзеров
-//let newForm = document.getElementById('newUserForm') //форма для создания юзера
-
-
-
-function getAdminInfo() {
-    console.log('Загружаю страницу администратора...')
-    fetch(urlInfo)
-        .then((res) => res.json())
-        .then(data => {
-            showAdminPage(data)
-            console.log("Страница админа загружена")
-        }).catch((ex) => console.error(ex))
-}
-
-function showAdminPage(userAdmin) {
-    let result = '';
-debugger
-    result += `<tr>
-            <td>${userAdmin.id}</td>
-            <td>${userAdmin.name}</td>
-            <td>${userAdmin.lastName}</td>
-            <td>${userAdmin.age}</td>
-            <td>${userAdmin.email}</td>
-            <td>${userAdmin.username}</td>
-            <td>${userAdmin.roles.map(role => " " + role.role.substring(5))}</td>
-            </tr>`;
-    adminTable.innerHTML = result
-    navbarBrandAdmin.innerHTML = `<b><span>${userAdmin.username}</span></b>
-                             <span>with roles:</span>
-                             <span>${userAdmin.roles.map(role => " " + role.role.substring(5))}</span>`
-
-}
-
-getAdminInfo()
+// const urlInfo = 'http://localhost:8080/api/users/admin_info'
+// const navbarBrandAdmin = document.getElementById('navbarBrandAdmin') //хедер
+// const adminTable = document.getElementById('tableAdmin') //личная инфа админа
+// //таблица юзеров
+// //let newForm = document.getElementById('newUserForm') //форма для создания юзера
+//
+//
+//
+// function getAdminInfo() {
+//     console.log('Загружаю страницу администратора...')
+//     fetch(urlInfo)
+//         .then((res) => res.json())
+//         .then(data => {
+//             showAdminPage(data)
+//             console.log("Страница админа загружена")
+//         }).catch((ex) => console.error(ex))
+// }
+//
+// function showAdminPage(userAdmin) {
+//     let result = '';
+// debugger
+//     result += `<tr>
+//             <td>${userAdmin.id}</td>
+//             <td>${userAdmin.name}</td>
+//             <td>${userAdmin.lastName}</td>
+//             <td>${userAdmin.age}</td>
+//             <td>${userAdmin.email}</td>
+//             <td>${userAdmin.username}</td>
+//             <td>${userAdmin.roles.map(role => " " + role.role.substring(5))}</td>
+//             </tr>`;
+//     adminTable.innerHTML = result
+//     navbarBrandAdmin.innerHTML = `<b><span>${userAdmin.username}</span></b>
+//                              <span>with roles:</span>
+//                              <span>${userAdmin.roles.map(role => " " + role.role.substring(5))}</span>`
+//
+// }
+//
+// getAdminInfo()
 
 const URL = 'http://localhost:8080/api/users'
 const usersTable = document.getElementById('usersTable')
@@ -53,17 +52,12 @@ async function getAllUsers() {
         console.error('Ошибка вывода таблицы: ', er)
     }
 }
+
 function showTable(users) {
 
     let result = ''
     for (let user of users) {
-      //  let stringRole = ''
-           const stringRole = user.roles.map(el => el.role.replace('ROLE_', ' ')).join(', ')
-      //  for (let el of user.roles) {
-      //      stringRole = getStringRole(user.roles)
-          //  debugger
-            //   stringRole += role.role + "<br>"
-     //   }
+        const stringRole = user.roles.map(el => el.role.replace('ROLE_', ' ')).join(', ')
         result += `
             <tr>
                 <td>${user.id}</td>
@@ -87,4 +81,64 @@ function showTable(users) {
             </tr>`
     }
     usersTable.innerHTML = result
+}
+
+
+let list = [
+    {id: 1, role: "ROLE_USER"},
+    {id: 2, role: "ROLE_ADMIN"}
+]
+
+createNewUser()
+
+
+//выбор роли из выпадающего списка из newForm
+function checkRolesForNewAndEdit() {
+    let roles = []
+    let options = document.querySelector('#newRoles').options
+    console.log(options)
+    for (let i = 0; i < options.length; i++) {
+        if (options[i].selected) {
+            roles.push(list[i])
+        }
+    }
+    return roles
+}
+
+//Сохранение юзера
+// Отправка заполненной формы на сервер
+//Предотвращение стандартного поведения браузера при отправке формы с помощью метода e.preventDefault()
+// позволяет обработать отправку данных формы программным способом, без перезагрузки или перенаправления страницы.
+// В данном случае, это позволяет отправить POST запрос на указанный URL с помощью JavaScript, обработать ответ от сервера
+// и обновить список пользователей без перезагрузки страницы.
+function createNewUser() {
+    newForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        fetch('http://localhost:8080/api/users',
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json;charset=utf-8'
+                },
+                body: JSON.stringify({
+
+                    name: document.getElementById('newName').value,
+                    lastName: document.getElementById('newLastName').value,
+                    age: document.getElementById('newAge').value,
+                    email: document.getElementById('newEmail').value,
+                    username: document.getElementById('newUsername').value,
+                    password: document.getElementById('newPassword').value,
+                    roles: checkRolesForNewAndEdit()
+                })
+            })
+            .then((response) => {
+                if (response.ok) {
+                    newForm.reset() //очищаем форму
+                    document.getElementById('nav-users-tab').click() //клик по вкладке Users Table
+                    console.log('Новый пользователь загружен')
+                    getAllUsers() //обновляем таблицу пользователей
+                }
+            }).catch(ex => console.error('Ошибка отправки формы: ', ex))
+    })
 }
