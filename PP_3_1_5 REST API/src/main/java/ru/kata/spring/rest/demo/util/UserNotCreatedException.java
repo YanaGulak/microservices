2874cf1ed70e724c3faf31e3574cab125d581d41
Yana <yana_mashuta@mail.ru>
2024-05-31 13:21:56 +0500

    first commit (eureka-server)

--- PP_3_1_5 REST API/src/main/java/ru/kata/spring/rest/demo/util/UserNotCreatedException.java
+++
@@ -1,8 +0,0 @@
-package ru.kata.spring.rest.demo.util;
-
-public class UserNotCreatedException extends RuntimeException {
-
-    public UserNotCreatedException(String message) {
-        super(message);
-    }
-}
--- PP_3_1_5 REST API/src/main/java/ru/kata/spring/rest/demo/util/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package ru.kata.spring.rest.demo.util;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
